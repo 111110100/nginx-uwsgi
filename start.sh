a0f51bfb44c5f0f523232d7f57e1e9c2815612a9
111110100 <elomibao@gmail.com>
2024-08-01 15:49:50 +1000

    Added optional settings to the kernel

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# echo "1024 65535" > /proc/sys/net/ipv4/ip_local_port_range
+# echo "3" > /proc/sys/net/ipv4/tcp_fin_timeout
+# echo "1" > /proc/sys/net/ipv4/tcp_tw_recycle
+# echo "0" > /proc/sys/net/ipv4/tcp_syncookies
+
 uwsgi --ini mysite_uwsgi.ini
